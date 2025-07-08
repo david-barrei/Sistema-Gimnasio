060ba95f51519edcb7a32c1bdbe327452a469b33
david-barrei <davbarreiro4@gmail.com>
2025-07-07 22:21:51 -0500

    definiendo endpoint ventas x mes

--- sales/views.py
+++ sales/views.py
@@ -5,6 +5,13 @@ from rest_framework import viewsets, permissions
 from rest_framework.generics import CreateAPIView
 from .serializers import ProductSerializers,SaleWriteSerializer,SaleReadSerializer
 from .models import Product,Sale
+from django.db.models.functions import TruncMonth
+from django.db.models import Sum
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+
 
 class ProductViews(CreateAPIView):
 
@@ -23,3 +30,15 @@ class SaleViews(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def sales_by_month(request):
+    qs = (
+        Sale.objects.annotate(month=TruncMonth("date"))
+        .values("month")
+        .annotate(total=Sum("total"))
+        .order_by("month")
+    ) # formatea como [{ "month": "2025-01", "total": 1234.50 }, …]
+    data = [{"month": g["month"].strftime("%Y-%m"), "total":g["total"]} for g in qs]
+    return Response(data)
