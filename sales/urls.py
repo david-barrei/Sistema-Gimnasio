5d0c94f6bdf6d210059cedd464994097b607bcea
david-barrei <davbarreiro4@gmail.com>
2025-07-09 21:22:27 -0500

    agregando rutas sales y stock

--- sales/urls.py
+++ sales/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProductViews,SaleViews
+from .views import ProductViews,SaleViews,sales_by_month,low_stock_alert
 
 
 app_name= 'sales'
@@ -10,6 +10,9 @@ router.register(r'sales', SaleViews, basename='sale')
 
 urlpatterns = [
     path('product/',ProductViews.as_view(),name='product'),
+    path('sales-month/',sales_by_month(),name='sales_by_month'),
+    path('low_stock/',low_stock_alert.as_view(),name='stock'),
+
 ]+ router.urls
 
 
